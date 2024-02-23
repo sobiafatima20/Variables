5dc88a7501e755be473d5346a88909ebb0bc2f7e
Sobia Fatima <133638285+sobiafatima20@users.noreply.github.com>
2024-02-23 20:47:34 +0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,10 @@
+//Variable 
+//1. let
+var name1 = "Sobia";
+console.log(name1);
+//2. var
+var asd = 7;
+console.log(asd);
+//3. const never change 
+var frndname = "Fatima";
+console.log(frndname);
---
+++ variables.ts
@@ -0,0 +1,10 @@
+//Variable 
+//1. let
+let name1 : string = "Sobia";
+console.log(name1)
+//2. var
+var asd : number = 7;
+console.log(asd)
+//3. const never change 
+const frndname : string = "Fatima";
+console.log(frndname)
